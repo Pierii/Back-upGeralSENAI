1abf9897668332454a224376a3a98e63a37cde68
Rafael Pieri Almeida <46181942866@info.local>
2019-04-16 11:22:12 -0300

    16.04

---
+++ DesafioSENAIzinho/Aluno.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DesafioSENAIzinho {
+    public class Aluno {
+
+        public string nome;
+        public DateTime dataNascimento;
+        public string curso;
+        public string Aluno.remove(Aluno);
+        public int numeroSala;
+    }
+}
---
+++ DesafioSENAIzinho/Sala.cs
@@ -0,0 +1,10 @@
+
+
+namespace DesafioSENAIzinho {
+    public class Sala {
+        public int numeroSala;
+        public int capacidadeAtual;
+        public int capacidadeTotal;
+        public string[] alunos;
+    }
+}
